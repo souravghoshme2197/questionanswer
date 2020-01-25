8d19af2f3ddefbfb2a3bbc6fb47936dd32b1d985
Sourav Ghosh <souravghosh2197@gmail.com>
2020-01-26 00:11:28 +0530

    initial commit to start spring application

---
+++ src/main/java/com/springmvc/questionanswer/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.springmvc.questionanswer.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String getHome() {
+        return "Sourav Ghosh";
+    }
+}
---
+++ src/test/java/com/springmvc/questionanswer/QuestionanswerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvc.questionanswer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionanswerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
